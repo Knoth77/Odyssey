babc88da8fd7a5ba83eaf4bb83a74cce020d913b
Knoth77 <Knoth77@gmail.com>
2014-05-15 22:02:54 -0400

    Fixed include

--- SimpleSideScrollerFramework/FlamethrowerRayCast.h
+++ SimpleSideScrollerFramework/FlamethrowerRayCast.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "C:\Users\Matt\Source\Repos\Odyssey_Matt\SimpleSideScrollerFramework\Box2D\Dynamics\b2WorldCallbacks.h"
+#include "../SimpleSideScrollerFramework/Box2D/Dynamics/b2WorldCallbacks.h"
 class FlamethrowerRayCast :
 	public b2RayCastCallback
 {
